a34e818f86832542e3e950fb0d9ac1b1083bef7d
tkyg <80114433+tkyg@users.noreply.github.com>
2022-03-11 18:06:20 -0500

    commit tree 6e9c1cf94ec5cc009cb78f6125b53bf6e425c613

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length/2; i++){
+    const j = word.length -1 -i
+    if (word[i] !== word[j]) return false
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+
 */
 
 /*
@@ -18,8 +24,30 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+
 }
 
 module.exports = isPalindrome;
